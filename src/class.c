16432d50f4d844e2a35f31d55ffc92b126fea246
Uchio KONDO <udzura@udzura.jp>
2016-08-31 15:30:24 +0900

    Move class definition all to class.c

--- src/class.c
+++ src/class.c
@@ -4,14 +4,15 @@
 
 #include <raft.h>
 
-#define DECLARE_RAFT_TYPE(name, data_type_name, raft_type, free_func_name) \
-    typedef struct {                                                    \
-      raft_type *data;                                                  \
-    } data_type_name;                                                   \
-                                                                        \
-    static const struct mrb_data_type mrb_craft_ ## name ## _data_type = { \
-      #data_type_name, free_func_name,                                  \
-    };
+#define DECLARE_RAFT_TYPE(name, data_type_name, raft_type)              \
+  typedef struct {                                                      \
+    raft_type *data;                                                    \
+  } data_type_name;
+
+#define DECLARE_RAFT_CLEANUP(data_type_name, mrb_data_type_varname, free_func_name) \
+  static const struct mrb_data_type mrb_data_type_varname = {           \
+    #data_type_name, free_func_name,                                    \
+  };
 
 #define INITALIZE_RAFT_DATA(data, data_type_name, mrb_data_type_varname) \
     do {                                                                \
@@ -24,7 +25,28 @@
       data = (data_type_name *)mrb_malloc(mrb, sizeof(data_type_name)); \
     } while (0)
 
-DECLARE_RAFT_TYPE(entry, mrb_craft_entry_data, raft_entry_t, mrb_free);
+/* Core class declarations */
+
+DECLARE_RAFT_TYPE(server, mrb_craft_server_data, raft_server_t);
+static void mrb_craft_server_free(mrb_state *mrb, void* p)
+{
+  mrb_craft_server_data *srv = (mrb_craft_server_data *)p;
+  raft_free(srv->data);
+}
+DECLARE_RAFT_CLEANUP(mrb_craft_server_data, mrb_craft_server_data_type, mrb_craft_server_free);
+
+DECLARE_RAFT_TYPE(entry, mrb_craft_entry_data, raft_entry_t);
+DECLARE_RAFT_CLEANUP(mrb_craft_entry_data, mrb_craft_entry_data_type, mrb_free);
+
+static mrb_value mrb_craft_server_init(mrb_state *mrb, mrb_value self)
+{
+  mrb_craft_server_data *data;
+  INITALIZE_RAFT_DATA(data, mrb_craft_server_data, mrb_craft_server_data_type);
+
+  data->data = raft_new();
+  DATA_PTR(self) = data;
+  return self;
+}
 
 static mrb_value mrb_craft_entry_init(mrb_state *mrb, mrb_value self)
 {
@@ -37,9 +59,14 @@ static mrb_value mrb_craft_entry_init(mrb_state *mrb, mrb_value self)
   return self;
 }
 
-void mrb_mruby_craft_class_init(mrb_state *mrb, struct RClass *craft, struct RClass *entry)
+void mrb_mruby_craft_class_init(mrb_state *mrb,
+                                struct RClass *craft,
+                                struct RClass *server,
+                                struct RClass *entry)
 {
-  entry = mrb_define_class_under(mrb, craft, "Entry", mrb->object_class);
+  server = mrb_define_class_under(mrb, craft, "Server", mrb->object_class);
+  mrb_define_method(mrb, server, "initialize", mrb_craft_server_init, MRB_ARGS_NONE());
 
+  entry = mrb_define_class_under(mrb, craft, "Entry", mrb->object_class);
   mrb_define_method(mrb, entry, "initialize", mrb_craft_entry_init, MRB_ARGS_NONE());
 }
--- src/craft.c
+++ src/craft.c
@@ -14,57 +14,22 @@
 
 #define DONE mrb_gc_arena_restore(mrb, 0);
 
-void mrb_mruby_craft_class_init(mrb_state *mrb, struct RClass *craft, struct RClass *entry);
-
-typedef struct {
-  raft_server_t *srv;
-} mrb_craft_server_data;
-
-static void mrb_craft_server_free(mrb_state *mrb, void* p)
-{
-  mrb_craft_server_data *srv = (mrb_craft_server_data *)p;
-  raft_free(srv->srv);
-}
-
-static const struct mrb_data_type mrb_craft_server_data_type = {
-  "mrb_craft_server_data", mrb_craft_server_free,
-};
-
-static mrb_value mrb_craft_server_init(mrb_state *mrb, mrb_value self)
-{
-  mrb_craft_server_data *data;
-
-  data = (mrb_craft_server_data *)DATA_PTR(self);
-  if (data) {
-    raft_free(data->srv);
-    mrb_free(mrb, data);
-  }
-  DATA_TYPE(self) = &mrb_craft_server_data_type;
-  DATA_PTR(self) = NULL;
-
-  data = (mrb_craft_server_data *)mrb_malloc(mrb, sizeof(mrb_craft_server_data));
-  data->srv = raft_new();
-  DATA_PTR(self) = data;
-
-  return self;
-}
-
-static mrb_value mrb_craft_hello(mrb_state *mrb, mrb_value self)
-{
-  mrb_craft_server_data *srv = DATA_PTR(self);
-
-  return mrb_str_new_lit(mrb, "Hello");
-}
+void mrb_mruby_craft_class_init(mrb_state *mrb,
+                                struct RClass *craft,
+                                struct RClass *server,
+                                struct RClass *entry);
 
 void mrb_mruby_craft_gem_init(mrb_state *mrb)
 {
-  struct RClass *craft, *server, *entry;
+  struct RClass *craft,
+    *server,
+    *entry;
+
   craft = mrb_define_module(mrb, "CRaft");
-  server = mrb_define_class_under(mrb, craft, "Server", mrb->object_class);
-
-  mrb_define_method(mrb, server, "initialize", mrb_craft_server_init, MRB_ARGS_NONE());
-
-  mrb_mruby_craft_class_init(mrb, craft, entry);
+  mrb_mruby_craft_class_init(mrb,
+                             craft,
+                             server,
+                             entry);
 
   DONE;
 }
